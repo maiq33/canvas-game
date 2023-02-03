3576385274be5df5532a98511de3a464ee0b7d08
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-02-03 03:27:06 +0000

    avance hasta el minuto 15:28

--- index.js
+++ index.js
@@ -5,4 +5,26 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-console.log(c);
+class Player  {
+    constructor(x,y,radius,color){
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+
+    draw () {
+        c.beginPath
+        c.arc(this.x,this.y,this.radius,0,Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    
+}
+
+const player = new Player(100,100,30,'blue');
+
+player.draw()
+
+console.log(player);
